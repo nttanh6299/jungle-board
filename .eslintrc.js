1f87738d0a46c121d98bd75695fa73fecb5966fa
Anh.Tuan <nttanh6299@gmail.com>
2022-03-12 17:19:10 +0700

    hooks eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   parserOptions: {
