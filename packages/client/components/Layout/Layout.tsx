768551bc9a20210c727ce0f29d148fe6f71c2fab
Anh.Tuan <nttanh6299@gmail.com>
2023-01-25 15:24:46 +0700

    add router loading

--- packages/client/components/Layout/Layout.tsx
+++ packages/client/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import { PropsWithChildren, useEffect } from 'react'
 import { useSession, signOut } from 'next-auth/react'
 import useMe from 'hooks/useMe'
-import AppLoading from 'components/AppLoading'
 import { setAccessToken } from 'utils'
 import useIsomorphicLayoutEffect from 'hooks/useIsomorphicLayoutEffect'
 import { notify, subscribe, unsubscribe } from 'utils/subscriber'
 import { NotifyEvent } from 'constants/enum'
-import ConnectionAlert from 'components/ConnectionAlert'
 
 const Layout = ({ children }: PropsWithChildren<unknown>) => {
   const { getMe, restartFetchUser } = useMe()
@@ -34,15 +32,11 @@ const Layout = ({ children }: PropsWithChildren<unknown>) => {
   })
 
   return (
-    <>
-      <ConnectionAlert />
-      <AppLoading />
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:max-w-[848px] md:max-h-[638px] w-full h-full md:p-2">
-        <div className="relative sm:p-4 p-3 bg-white md:rounded-2xl shadow-tight shadow-primary w-full h-full flex flex-col">
-          {children}
-        </div>
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:max-w-[848px] md:max-h-[638px] w-full h-full md:p-2">
+      <div className="relative sm:p-4 p-3 bg-white md:rounded-2xl shadow-tight shadow-primary w-full h-full flex flex-col">
+        {children}
       </div>
-    </>
+    </div>
   )
 }
 
